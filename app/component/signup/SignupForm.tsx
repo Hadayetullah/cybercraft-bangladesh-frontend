ccfee75769194f9b055856c99879473fb52c660b
Hadayetullah <hadayetullah30@gmail.com>
2025-03-18 23:09:59 +0600

    Create sign up page

---
+++ app/component/signup/SignupForm.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+
+const SignupForm = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleChange = (e: any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFocus = () => {
+    setError("");
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (formData.name === "") {
+      setError("name");
+    } else if (formData.email === "") {
+      setError("email");
+    } else if (formData.password === "") {
+      setError("password");
+    } else if (formData.password.length < 8) {
+      setError("password_length");
+    } else if (formData.confirm_password === "") {
+      setError("confirm_password");
+    } else if (formData.password !== formData.confirm_password) {
+      setError("password_mismatch");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <form className="w-full sm:w-[380px] md:w-[410px] lg:w-[440px] 2xl:w-[487px] flex flex-col space-y-1 lg:space-y-2 2xl:space-y-3 pt-2 md:pt-4 xl:pt-5">
+        <div className="w-full">
+          <label
+            htmlFor="name"
+            className="block text-[13px] lg:text-[14px] leading-[125%] tracking-[0%] text-[#345485]"
+          >
+            Full Name
+          </label>
+
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full name"
+            value={formData.name}
+            onChange={(e) => handleChange(e)}
+            onFocus={() => handleFocus()}
+            required
+            className="w-full h-[35px] sm:h-[45px] xl:h-[56px] p-[10px] bg-[#FFFFFF] text-[16px] leading-[125%] tracking-[0%] text-[#00000080] mt-1 border border-[#D8DADC] rounded-[10px] outline-none"
+          />
+
+          <div
+            className={`text-red-500 text-xs italic w-full h-[16px] ${
+              error === "name" ? "visible" : "invisible"
+            }`}
+          >
+            <p>Please enter your name</p>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <label
+            htmlFor="email"
+            className="block text-[13px] lg:text-[14px] leading-[125%] tracking-[0%] text-[#345485]"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="example@gmail.com"
+            value={formData.email}
+            onChange={(e) => handleChange(e)}
+            onFocus={() => handleFocus()}
+            required
+            className="w-full h-[35px] sm:h-[45px] xl:h-[56px] p-[10px] bg-[#FFFFFF] text-[16px] leading-[125%] tracking-[0%] text-[#00000080] mt-1 border border-[#D8DADC] rounded-[10px] outline-none"
+          />
+
+          <div
+            className={`text-red-500 text-xs italic w-full h-[16px] ${
+              error === "email" ? "visible" : "invisible"
+            }`}
+          >
+            <p>Please enter your email address</p>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <label
+            htmlFor="password"
+            className="block text-[13px] lg:text-[14px] leading-[125%] tracking-[0%] text-[#345485]"
+          >
+            Create a password
+          </label>
+
+          <input
+            type="password"
+            name="password"
+            placeholder="must be 8 characters"
+            value={formData.password}
+            onChange={(e) => handleChange(e)}
+            onFocus={() => handleFocus()}
+            required
+            className="w-full h-[35px] sm:h-[45px] xl:h-[56px] p-[10px] bg-[#FFFFFF] text-[16px] leading-[125%] tracking-[0%] text-[#00000080] mt-1 border border-[#D8DADC] rounded-[10px] outline-none"
+          />
+
+          <div
+            className={`text-red-500 text-xs italic w-full h-[16px] ${
+              error === "password" || "password_length"
+                ? "visible"
+                : "invisible"
+            }`}
+          >
+            <p>
+              {error === "password" && "Please enter password"}
+              {error === "password_length" &&
+                "Password must be at least 8 characters long"}
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <label
+            htmlFor="confirm_password"
+            className="block text-[13px] lg:text-[14px] leading-[125%] tracking-[0%] text-[#345485]"
+          >
+            Confirm password
+          </label>
+          <input
+            type="password"
+            name="confirm_password"
+            placeholder="repeat password"
+            value={formData.confirm_password}
+            onChange={(e) => handleChange(e)}
+            onFocus={() => handleFocus()}
+            required
+            className="w-full h-[35px] sm:h-[45px] xl:h-[56px] p-[10px] bg-[#FFFFFF] text-[16px] leading-[125%] tracking-[0%] text-[#00000080] mt-1 border border-[#D8DADC] rounded-[10px] outline-none"
+          />
+
+          <div
+            className={`text-red-500 text-xs italic w-full h-[16px] ${
+              error === "confirm_password" || "password_mismatch"
+                ? "visible"
+                : "invisible"
+            }`}
+          >
+            <p>
+              {error === "confirm_password" && "Please repeat password"}
+              {error === "password_mismatch" && "Passwords do not match"}
+            </p>
+          </div>
+        </div>
+
+        <button
+          onSubmit={(e) => handleSubmit(e)}
+          onClick={(e) => handleSubmit(e)}
+          className="w-full h-[35px] sm:h-[45px] xl:h-[56px] cursor-pointer leading-[125%] tracking-[0%] text-[16px] text-[#FFFFFF] font-[600] text-center bg-[#345485] rounded-[10px]"
+        >
+          Create account
+        </button>
+
+        <h2 className="text-[18px] text-[#000000B2] font-[600] text-center py-2 sm:py-4 leading-[125%] tracking-[0%] ">
+          Or
+        </h2>
+
+        <div className="w-full flex flex-row items-center justify-center gap-x-2">
+          <span className="text-[#000000B2] text-[14px] leading-[125%] tracking-[0%]">
+            Already have an account?
+          </span>
+
+          <Link
+            href={"/login"}
+            className="font-[600] text-[#345485] underline text-[14px] text-center leading-[125%] tracking-[0%]"
+          >
+            Log in
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
---
+++ app/signup/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Navbar from "../component/navbar/Navbar";
+import Image from "next/image";
+import SignupForm from "../component/signup/SignupForm";
+
+const Page = () => {
+  return (
+    <>
+      <Navbar />
+      <div
+        className="w-full pt-[100px] sm:pt-[0px] h-[110vh] sm:h-[100vh] px-4 sm:px-0"
+        style={{
+          background:
+            "linear-gradient(144.42deg, rgba(194, 218, 255, 0.96) 20.85%, rgba(239, 241, 240, 0.17) 134.64%)",
+        }}
+      >
+        <div className="w-full h-full xl:w-[1084px] mx-auto">
+          <div className="w-full h-full flex flex-col sm:flex-row items-center justify-evenly sm:gap-x-10 sm:px-4">
+            <div className="w-full flex flex-col items-center gap-y-7">
+              <img
+                src={"/image/CyberCraft-Bangladesh.png"}
+                alt="CyberCraft-Bangladesh-logo"
+                className="w-[180px] sm:w-[294px] h-[80px] sm:h-[129px] "
+              />
+
+              <p className="text-[#353535] text-[18px] sm:text-[20px] text-center leading-[100%] tracking-[0%] ">
+                Welcome back to CyberCraft Bangladesh, <br /> where your
+                creativity thrives
+              </p>
+            </div>
+
+            <div className="w-full h-full pt-5 sm:pt-7">
+              <SignupForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
